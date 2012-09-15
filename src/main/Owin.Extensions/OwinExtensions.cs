ba5719128486cee4ee8ea5a8d87d378cc2fe1ace
prabirshrestha <mail@prabir.me>
2012-09-15 19:59:46 -0400

    empty OwinExtensions class

---
+++ src/main/Owin.Extensions/OwinExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Owin.Extensions
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public static class OwinExtensions
+    {
+         
+    }
+}
