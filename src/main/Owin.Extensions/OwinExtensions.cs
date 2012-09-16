f71cc00dabc228be0fdad738ae5f40162c3b074d
prabirshrestha <mail@prabir.me>
2012-09-15 20:01:01 -0400

    added StartupEnv/Env and WsEnv alias

--- src/main/Owin.Extensions/OwinExtensions.cs
+++ src/main/Owin.Extensions/OwinExtensions.cs
@@ -6,8 +6,11 @@ namespace Owin.Extensions
 {
     using AppFunc = Func<IDictionary<string, object>, Task>;
 
+    using StartupEnv = System.Collections.Generic.IDictionary<string, object>;
+    using Env = System.Collections.Generic.IDictionary<string, object>;
+    using WsEnv = System.Collections.Generic.IDictionary<string, object>;
+
     public static class OwinExtensions
     {
-         
     }
 }
